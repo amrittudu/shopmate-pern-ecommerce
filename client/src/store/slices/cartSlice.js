ef19496e88634e1e881af9594d698159b97f2b0a
amrittudu <amrittudu987@gmail.com>
2026-05-02 18:02:01 +0530

    second commit

--- client/src/store/slices/cartSlice.js
+++ client/src/store/slices/cartSlice.js
@@ -5,7 +5,27 @@ const cartSlice = createSlice({
   initialState: {
     cart: [],
   },
-  reducers: {},
+  reducers: {
+    addToCart(state, action) {
+      const {product, quantity} = action.payload;
+      const existingItem = state.cart.find( (item) => item.product.id === product.id);
+      
+      if(existingItem) {
+        existingItem.quantity += quantity;
+      } 
+      else {
+        state.cart.push( {product, quantity} );
+      }
+    },
+    removeFromCart(state, action ) {
+      state.cart = state.cart.filter( (item) => item.product.id !== action.payload.id );
+    },
+
+    updateCartQuantity(state, action) {
+      const item = state.cart.find( (item) => item.product.id ===)
+    }
+
+  },
 });
 
 export const {} = cartSlice.actions;
