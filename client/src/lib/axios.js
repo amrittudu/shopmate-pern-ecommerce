ba2d94e45672f384db77248dd7a5c02e68699d9a
amrittudu <amrittudu987@gmail.com>
2026-09-26 13:47:28 +0530

    axios update

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   
-  baseURL:
-    import.meta.env.MODE === "development"
+  baseURL: import.meta.env.DEV 
       ? "http://localhost:4000/api/v1"
-      : "/",
+      : "https://shopmate-client-ft6w.onrender.com/api/v1",
+
   withCredentials: true,
+
 });
